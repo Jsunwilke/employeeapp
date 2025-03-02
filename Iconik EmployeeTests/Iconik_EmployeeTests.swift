cc7327f6ab2a680296e48ce30b1797612fb68fc3
Jason Wilkey <jsunwilke@gmail.com>
2025-03-02 13:28:33 -0500

    Initial Commit

---
+++ Iconik EmployeeTests/Iconik_EmployeeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Iconik_EmployeeTests.swift
+//  Iconik EmployeeTests
+//
+//  Created by administrator on 3/2/25.
+//
+
+import Testing
+@testable import Iconik_Employee
+
+struct Iconik_EmployeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Iconik EmployeeUITests/Iconik_EmployeeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Iconik_EmployeeUITestsLaunchTests.swift
+//  Iconik EmployeeUITests
+//
+//  Created by administrator on 3/2/25.
+//
+
+import XCTest
+
+final class Iconik_EmployeeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
